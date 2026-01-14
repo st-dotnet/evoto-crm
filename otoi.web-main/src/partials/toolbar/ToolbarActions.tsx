092c6da7d20846d8b36993863664ef02c0887b93
st-dotnet <stdev.net@gmail.com>
2026-01-14 19:15:09 +0530

    To make an Evoto UI responsive

--- otoi.web-main/src/partials/toolbar/ToolbarActions.tsx
+++ otoi.web-main/src/partials/toolbar/ToolbarActions.tsx
@@ -1,7 +1,7 @@
 import { IToolbarActionsProps } from './types';
 
 const ToolbarActions = ({ children }: IToolbarActionsProps) => {
-  return <div className="flex items-center gap-2.5">{children}</div>;
+  return <div className="flex items-center flex-wrap gap-2 lg:gap-2.5">{children}</div>;
 };
 
 export { ToolbarActions };
